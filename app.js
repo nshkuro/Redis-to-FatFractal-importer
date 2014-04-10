e7321222b5b6e20236cdaf2d7a706d3599f6af36
nshkuro <nikolay@shkuro.net>
2014-04-10 16:29:40 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,56 @@
+var redis   = require("redis"),
+    winston = require('winston'),
+    config = require("./config/config.json"),
+    logger = new (winston.Logger)({    
+    transports: [      
+        new (winston.transports.Console)({colorize: true}),
+        new (winston.transports.File)({ filename: 'app.log' })
+      ]
+    }),
+    FF = require("./lib/FF"),
+    FatFractal = new FF(config.FatFractal.username, config.FatFractal.password, config.FatFractal.url);
+
+logger.info('Application is run');
+
+var clientSubscribe = redis.createClient(config.redis.port, config.redis.host);
+var client = redis.createClient(config.redis.port, config.redis.host);
+
+clientSubscribe.on("error", function (err) {
+  logger.error(err);
+});
+
+client.on("error", function (err) {
+  logger.error(err);
+});
+
+if (clientSubscribe && client) {
+  logger.info("Redis: connected to host: %s, port: %s", clientSubscribe.host, clientSubscribe.port);
+}
+
+clientSubscribe.subscribe(config.redis.channel);
+
+logger.info("Redis: subscribe on channel: %s", config.redis.channel);
+
+clientSubscribe.on("message", function(channel, key){
+    logger.info("channel : %s, the message : %s", channel, key);
+    
+    client.hgetall(key, function(err, object) {
+      if (err) {
+        logger.error(err);  
+      }
+
+      object.key = key;  
+      var key_arr = key.split(':');
+      var collection = key_arr[0];  
+
+      FatFractal.saveObject(collection, object, function(obj, msg) {        
+        if (typeof obj != 'object') {
+          logger.error(obj);
+          logger.error(msg);
+        } else {
+          logger.info(msg);  
+        }        
+      });
+    });
+
+});
---
+++ readme.md
@@ -0,0 +1,11 @@
+
+1. Installation
+Install all dependencies. Run command 
+"npm install ." 	
+
+2. Configuration
+You can configure application via edit config/config.json file
+
+3. Running
+Run application, for example run command 
+"node app.js"
